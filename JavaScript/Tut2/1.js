ff07225cf13b3c04aa2053d56829b926e2198f52
Uttam Kumar <uk89197@gmail.com>
2025-01-16 11:42:52 +0530

    Printing on console

---
+++ JavaScript/Tut2/1.js
@@ -0,0 +1,10 @@
+console.log("hello");
+console.log("chai");
+
+
+// ye bhi print karta hai but same line me
+process.stdout.write("hello");
+process.stdout.write("world");
+
+console.table({ city: "Patna" })
+console.warn("this is warning");
