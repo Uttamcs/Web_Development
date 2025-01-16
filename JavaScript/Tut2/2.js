4e2b9adad921ddd123473d07bb2364b486a46336
Uttam Kumar <uk89197@gmail.com>
2025-01-16 11:52:12 +0530

    Datatypes

---
+++ JavaScript/Tut2/2.js
@@ -0,0 +1,30 @@
+
+/* console.log("heloo");
+console.log("heloo");
+
+*/
+
+// console.log("heloo");
+
+// var score = 500
+let score = 500
+console.log(score);
+
+
+let name = "uttamRoy"
+let age = 20
+let boolean = false
+
+// object 
+let teaTypes = ["lemon tea", "orange tea", "oolong tea"]
+let user = {
+    name: "uttam roy",
+    age: 20,
+    boolean: false,
+    teaTypes: ["lemon tea", "orange tea", "oolong tea"]
+}
+console.log(user);
+
+let getScore = score
+
+console.log(getScore);
