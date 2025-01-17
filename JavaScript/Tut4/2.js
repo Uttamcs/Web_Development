965fadd3cba03f65671ed9935d4eeeedf012b9ff
Uttam Kumar <uk89197@gmail.com>
2025-01-17 10:28:46 +0530

    Control Statements

---
+++ JavaScript/Tut4/2.js
@@ -0,0 +1,43 @@
+// Control Statements in JS
+// 1. Conditional Statements
+// 2. Looping Statements
+
+// Conditional Statements
+// 1. if-else
+// 2. switch-case
+
+// // 1. if-else
+// let age = 18;
+// if (age >= 18) {
+//   console.log("You are eligible to vote");
+// } else {
+//   console.log("You are not eligible to vote");
+// }
+
+// // 2. switch-case
+// let day = 3;
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Invalid day");
+// }
