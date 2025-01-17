0767e25d987827d4102d3c43da9d52f74fe9b8ea
Uttam Kumar <uk89197@gmail.com>
2025-01-17 10:25:02 +0530

    Operations in JS

---
+++ JavaScript/Tut3/1.js
@@ -0,0 +1,18 @@
+let score = 5;
+let bomus = 543;
+
+let total = score + bomus;
+console.log(total);
+
+let addition = 4 + 5;
+let subtraction = 4 - 5;
+let multiplication = 4 * 5;
+let division = 4 / 5;
+let modulus = 4 % 5;
+let exponentiation = 4 ** 5;
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+console.log(modulus);
+console.log(exponentiation);
