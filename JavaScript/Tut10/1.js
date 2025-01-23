8c9222d1c4bfcf5b9c7d54c7be7b59d48408ece6
Uttam Kumar <uk89197@gmail.com>
2025-01-23 15:56:55 +0530

    DOM and BOM Manipulation in javascript

---
+++ JavaScript/Tut10/1.js
@@ -0,0 +1,28 @@
+// DOM and BOM Manipulation in javascript
+
+// 1. Document Object Model (DOM)
+// 2. Browser Object Model (BOM)
+
+// 1. Document Object Model (DOM)
+// - It is a programming interface for web documents.
+// - It represents the structure of the document as a tree.
+// - It provides a way to interact with the structure of the document.
+// - It is used to manipulate the structure of the document.
+// - It is used to manipulate the content of the document.
+// - It is used to manipulate the style of the document.
+// - It is used to manipulate the behaviour of the document.
+// - It is used to manipulate the events of  the document.
+
+// 2. Browser Object Model (BOM)
+// - It is a programming interface for web browsers.
+// - It represents the browser as an object.
+// - It provides a way to interact with the browser.
+// - It is used to manipulate the browser.
+// - It is used to manipulate the history of the browser.
+// - It is used to manipulate the location of the browser.
+// - It is used to manipulate the navigator of the browser.
+// - It is used to manipulate the screen of the browser.
+// - It is used to manipulate the window of the browser.
+
+console.log("Hello javascript");
+console.log(window);
