77b7b56835cfa4e15f3e1910f8ad13b11aefaa31
Uttam Kumar <uk89197@gmail.com>
2025-01-24 16:18:10 +0530

    Asynchronous Javascript with event loop

---
+++ JavaScript/Tut13/index.js
@@ -0,0 +1,14 @@
+console.log("Hello Uttam Bhai");
+
+
+function askHaalChal(){
+    console.log("Hello Uttam Bhai... Kaise ho?");
+}
+
+setTimeout(askHaalChal, 5000);
+console.log("Badhiya ho");
+
+
+for (let index = 0; index < 10; index++) {
+    console.log("Index: ", index);
+}
