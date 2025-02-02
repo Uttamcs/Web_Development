d7c30aeed79ac5f696dd5fb521eb0195a38a375f
Uttam Kumar <uk89197@gmail.com>
2025-02-02 18:26:48 +0530

    Tailwind in React

---
+++ React_by_Thapa/Tut16/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NetflixSeries from './Components/NetflixSeries'
+
+function App() {
+
+  return (
+    <>
+      <NetflixSeries/>
+    </>
+  )
+}
+
+export default App
---
+++ React_by_Thapa/Tut16/src/Components/SeriesCard.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styles from "./SeriesCard.module.css";
+import styled from "styled-components";
+
+const ApnaBtn = styled.button`
+  background-color: ${(props) => (props.super_hit ? "red" : "green")};
+  color: white;
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin: 10px;
+  cursor: pointer;
+  `;
+  
+  const KhudKaRating = styled.span`
+  color: black;
+  background-color: ${(props) => (props.super_hit ? "red" : "green")};
+  border-radius: 5px;
+  padding: 5px 10px;
+`;
+
+function SeriesCard(props) {
+  return (
+    <div className={styles.card_data}>
+      <div style={{ padding: "10px" }}>
+        <img
+          className={styles.card_img}
+          src={props.data.img_url}
+          alt={props.data.name}
+          width="40%"
+          height="40%"
+        />
+      </div>
+      <div className={styles.card_content}>
+        <h1>{props.data.name}</h1>
+        <h3>
+          {/* <span
+            className={`${styles.btn_atr} ${props.data.rating >= 8.5 ? styles.super_hit : styles.average
+              }`}
+          >
+            Rating:{props.data.rating}
+          </span> */}
+          <KhudKaRating super_hit={props.data.rating >= 8.5}>
+            Rating:{props.data.rating}
+          </KhudKaRating>
+        </h3>
+        <p>{props.data.description}</p>
+        <p className="bg-gray-900 text-yellow-300 rounded-sm">Genre: {props.data.genre}</p>
+        <a href={props.data.watch_url}>
+          {/* <button className={`${styles.btn_atr} ${props.data.rating >= 8.5 ? styles.super_hit : styles.average}`}>Watch now</button> */}
+          <ApnaBtn super_hit={props.data.rating >= 8.5}>Watch now</ApnaBtn>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default SeriesCard;
