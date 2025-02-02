881c8cd73d62ece1de29135619a022e12fa32c85
Uttam Kumar <uk89197@gmail.com>
2025-02-02 10:00:30 +0530

    Inline CSS

--- React_by_Thapa/Tut12/src/App.jsx
+++ React_by_Thapa/Tut12/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import NetflixSeries from './Components/NetflixSeries'
+import './App.css'
 
 const App = () => {
   return (
     <div>
       <NetflixSeries />
     </div>
-  )
+  );
 }
 
 export default App
--- React_by_Thapa/Tut12/src/Components/SeriesCard.jsx
+++ React_by_Thapa/Tut12/src/Components/SeriesCard.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import './SeriesCard.css'
+import React from "react";
+import "./SeriesCard.css";
 
 function SeriesCard(props) {
-    console.log(props);
-    
+  console.log(props);
+
   return (
-    <div>
-      <img
-        src={props.data.img_url}
-        alt={props.data.name}
-        width="40%"
-        height="40%"
-      />
-      <h1>{props.data.name}</h1>
-      <p>{props.data.description}</p>
-      <h3>Rating:{props.data.rating}</h3>
-      <a href={props.data.watch_url}>
-        <button>Watch now</button>
-      </a>
+    <div className="card-data">
+      <div class = {{padding:"10px"}}>
+        <img
+          className="card-img"
+          src={props.data.img_url}
+          alt={props.data.name}
+          width="40%"
+          height="40%"
+        />
+      </div>
+      <div className="card-content">
+        <h1>{props.data.name}</h1>
+        <p>{props.data.description}</p>
+        <h3>Rating:{props.data.rating}</h3>
+        <a href={props.data.watch_url}>
+          <button>Watch now</button>
+        </a>
+      </div>
     </div>
   );
 }
 
-export default SeriesCard
+export default SeriesCard;
